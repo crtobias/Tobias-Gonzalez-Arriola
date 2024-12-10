bc9ae2a3cd870bfc765668c17a0ffd12370185fd
crtobias <tgonzalezarriola@gmail.com>
2024-12-10 19:52:45 -0300

    flag

--- src/components/FlagButton.jsx
+++ src/components/FlagButton.jsx
@@ -1,16 +1,16 @@
 
-import flag from "../assets/Flag.svg";
-import flagsh from "../assets/FlagSh.svg";
+// import flag from "../../public/Flag.svg";
+// import flagsh from "../assets/FlagSh.svg";
 
 function FlagButton({ logo }) {
   return (
     <div
       className="relative h-12 w-20 flex justify-left pl-3 pb-2 items-center bg-contain bg-no-repeat transition-all duration-300"
       style={{
-        backgroundImage: `url(${flag})`,
+        backgroundImage: `url(/Flag.svg)`,
       }}
-      onMouseEnter={(e) => (e.currentTarget.style.backgroundImage = `url(${flagsh})`)}
-      onMouseLeave={(e) => (e.currentTarget.style.backgroundImage = `url(${flag})`)}
+      onMouseEnter={(e) => (e.currentTarget.style.backgroundImage = `url(/Flag.svg)`)} //asd
+      onMouseLeave={(e) => (e.currentTarget.style.backgroundImage = `url(/Flag.svg)`)}
     >
       <img className="h-8 w-8 " src={logo} alt="logo" />
     </div>
