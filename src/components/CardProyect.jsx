219c34472a075e5edafb1a6cc4ed59affb79c4c3
crtobias <tgonzalezarriola@gmail.com>
2025-02-20 15:30:03 -0300

    add es market

--- src/components/CardProyect.jsx
+++ src/components/CardProyect.jsx
@@ -1,23 +1,38 @@
 import Button from "./Button"
 
 export default function CardProyect({ proyecto }) {
-    const link = proyecto.git
-    return (
-        <div className="border border-black rounded pt-10 flex flex-col md:flex-row flex-grow gap-14 items-center p-6">
-            <img className=" h-60 w-60"
-             src={proyecto.img} alt="icon" />
-            <div className="max-w-[80vw] md:max-w-[50vw] text-gris">
-                <h2 className="p-1 text-2xl text-black">{proyecto.title}</h2>
-                <p className="p-1" >{proyecto.date}</p>
-                <p className="p-1">{proyecto.text}</p>
-                <p className="p-1"><p className="text-black">FrontEnd: </p>  {proyecto.stackFront}</p>
-                <p className="p-1"><p className="text-black">BackEnd:</p> {proyecto.stackBack}</p>
-                <br />
-                <a href={link} target="_blank" className="">
-                  <Button text="Github"></Button>
-                </a>
-
-            </div>
+  return (
+    <div className="border border-black rounded pt-10 flex flex-col md:flex-row flex-grow gap-14 items-center p-6">
+      <img className="h-60 w-60" src={proyecto.img} alt="icon" />
+      <div className="max-w-[80vw] md:max-w-[50vw] text-gris">
+        <h2 className="p-1 text-2xl text-black">{proyecto.title}</h2>
+        <p className="p-1">{proyecto.date}</p>
+        <p className="p-1">{proyecto.text}</p>
+        <p className="p-1">
+          <span className="text-black">FrontEnd: </span> {proyecto.stackFront}
+        </p>
+        <p className="p-1">
+          <span className="text-black">BackEnd: </span> {proyecto.stackBack}
+        </p>
+        <br />
+        <div className="flex gap-4">
+          {proyecto.git && (
+            <a href={proyecto.git} target="_blank" rel="noreferrer">
+              <Button text="Github" />
+            </a>
+          )}
+          {proyecto.deploy && (
+            <a href={proyecto.deploy} target="_blank" rel="noreferrer">
+              <Button text="Deploy" />
+            </a>
+          )}
+          {proyecto.yt && (
+            <a href={proyecto.yt} target="_blank" rel="noreferrer">
+              <Button text="Video" />
+            </a>
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/components/Screen3.jsx
+++ src/components/Screen3.jsx
@@ -3,22 +3,23 @@ import elmolino from "../assets/El-Molino.png"
 import login from "../assets/Login-Proyect.png"
 import Task from "../assets/task.jpeg"
 import habits from "../assets/habits.png"
+import esmarket from "../assets/Esmarket.png"
 export default function Screen3() {
 
-    const proyectoUno={
-        title:"Full Stack - El Molino",
-        text:"Developed a full-stack application to enhance restaurant experiences with an online menu system My role  focused on front-end development, including styles, components, and user experience.",
-        img:elmolino,
-        stackFront:"TypeScript, Redux Toolkit, React, HTML, CSS, Firebase",
-        stackBack:"TypeScript, Express.js, Firestore, MongoDB",
-        git:"https://github.com/crtobias/El-Molino",
-        date:"July 2023 - August 2023"
+    const proyectoUno = {
+        title: "Full Stack - El Molino",
+        text: "Developed a full-stack application to enhance restaurant experiences with an online menu system My role  focused on front-end development, including styles, components, and user experience.",
+        img: elmolino,
+        stackFront: "TypeScript, Redux Toolkit, React, HTML, CSS, Firebase",
+        stackBack: "TypeScript, Express.js, Firestore, MongoDB",
+        git: "https://github.com/crtobias/El-Molino",
+        date: "July 2023 - August 2023"
     }
 
     const proyectoHabits = {
         title: "Habits - React Native App",
         text: "Developed a mobile application designed to help users adopt and eliminate habits, focusing on personal growth and self-improvement. The app includes features like habit tracking, user authentication, and email verification.",
-        img: habits, 
+        img: habits,
         stackFront: "React Native, NativeWind, Expo Router, TailwindCSS",
         stackBack: "Node.js, Express.js, MongoDB, Prisma ORM, JWT, Nodemailer",
         git: "https://github.com/crtobias/Habits-native-Enero",
@@ -26,27 +27,37 @@ export default function Screen3() {
     };
 
 
-    const proyectoDos={
-        title:"Back End - Login/Register Template",
-        text:"Developed a reusable login and registration template for authentication systems. The project includes email verification, token-based authentication, and a scalable architecture for future integrations.",
-        img:login,
-        stackFront:"React, HTML, CSS, Javascript",
-        stackBack:"Javascript, Express.js, Prisma ORM, NodeMailer, PostgreSQL, JWT , Jest , SuperTest",
-        git:"https://github.com/crtobias/Template-Login",
-        date:"November 2024 - December 2024"
+    const proyectoDos = {
+        title: "Back End - Login/Register Template",
+        text: "Developed a reusable login and registration template for authentication systems. The project includes email verification, token-based authentication, and a scalable architecture for future integrations.",
+        img: login,
+        stackFront: "React, HTML, CSS, Javascript",
+        stackBack: "Javascript, Express.js, Prisma ORM, NodeMailer, PostgreSQL, JWT , Jest , SuperTest",
+        git: "https://github.com/crtobias/Template-Login",
+        date: "November 2024 - December 2024"
     }
 
-    const proyectoTres={
-        title:"CRUD Tasks",
-        text:"technical test for a Full-Stack developer position",
-        img:Task,
-        stackFront:"React, HTML, tailwind, Javascript, React Router, React Context, vercel",
-        stackBack:"Javascript , Node.JS , Express , MongoDB , Mongoose , mongoAtlas , express Validator , swagger , jest , supertest and Render deploy",
-        git:"https://github.com/crtobias/PT-Task-Frontend",
-        date:"December 2024"
+    const proyectoTres = {
+        title: "CRUD Tasks",
+        text: "technical test for a Full-Stack developer position",
+        img: Task,
+        stackFront: "React, HTML, tailwind, Javascript, React Router, React Context, vercel",
+        stackBack: "Javascript , Node.JS , Express , MongoDB , Mongoose , mongoAtlas , express Validator , swagger , jest , supertest and Render deploy",
+        git: "https://github.com/crtobias/PT-Task-Frontend",
+        date: "December 2024"
     }
 
 
+    const esMarketProject = {
+        title: "Es Market",
+        text: "Es Market is a free application for merchants and any type of market. With this app, you can manage your inventory, generate sales, create keys for employees, access sales history, and manage your own prices and products—all in an easy and cost-free way.",
+        img: esmarket,
+        stackFront: "React Native, Expo, NativeWind, React Context, TailwindCSS",
+        stackBack: "Firebase Authentication, Firestore, Firebase Storage",
+        deploy: "https://es-market-landing.vercel.app/",
+        date: "February 2025",
+        yt:"https://youtu.be/hbn6aZ1qfy4?si=byd-3fjCIzNip9cN",
+    };
 
 
     return (
@@ -55,10 +66,11 @@ export default function Screen3() {
             <h3 className="text-black text-4xl border-b border-black pl-9 pb-9">Projects</h3>
 
             <div className="flex flex-col p-9 gap-9 items-center ">
-                <CardProyect proyecto={proyectoHabits}/>
-                <CardProyect proyecto={proyectoTres}/>
-                <CardProyect proyecto={proyectoDos}/>
-                <CardProyect proyecto={proyectoUno}/>
+                <CardProyect proyecto={esMarketProject} />
+                <CardProyect proyecto={proyectoHabits} />
+                <CardProyect proyecto={proyectoTres} />
+                <CardProyect proyecto={proyectoDos} />
+                <CardProyect proyecto={proyectoUno} />
 
                 <p>Here are some of my highlighted projects. To explore more, feel free to visit my <a href="https://github.com/crtobias" target="blank" className="text-pinkk">GitHub 😀</a>.</p>
 
